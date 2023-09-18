dc05c8ebd61c20f8d262ede1fcea22ceadc75f7d
adam <adambrilian003@gmail.com>
2023-09-18 12:56:51 +0700

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from './database/data-source';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { dataSourceOptions } from './database/data-source';
 import { ConfigModule } from './config/config.module';
 import { GraphqlModule } from './graphql/graphql.module';
 import { AppResolver } from './app.resolver';
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 import { ValidationPipe } from '@nestjs/common';
-import dataSource from './database/data-source';
+// import dataSource from './database/data-source';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await dataSource.initialize();
+  // await dataSource.initialize();
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
   app.useGlobalPipes(
